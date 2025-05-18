2e417a8e5024879ebae9260c3208524377d3b22f
mattradial <137439375+mattradial@users.noreply.github.com>
2025-05-18 16:52:04 -0400

    feat: add logger init to main

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "http-job-que-system/server"
+import (
+	"http-job-que-system/logger"
+	"http-job-que-system/server"
+	"log"
+)
 
 func main() {
+	// Initialize logger
+	err := logger.InitLogger()
+	if err != nil {
+		log.Fatal("Logger initialization failed: ", err)
+	}
+	var log = logger.Log
+	log.Println("App started")
+
+	// Start server
 	server.Start()
 }
