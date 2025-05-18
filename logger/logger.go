8679f239147d410958600e903eee637e53f52ebf
mattradial <137439375+mattradial@users.noreply.github.com>
2025-05-18 17:07:44 -0400

    docs: add logger package docs

--- logger/logger.go
+++ logger/logger.go
@@ -1,3 +1,6 @@
+// Package logger provides a custom logger that writes application logs
+// to a file in the current working directory. It exposes a globally accessible
+// log.Logger instance that can be used throughout the application.
 package logger
 
 import (
@@ -8,23 +11,26 @@ import (
 
 const logFileName = "log.txt"
 
+// Log is the globally accessible logger instance. It is initialized by calling InitLogger.
 var Log *log.Logger
 
+// InitLogger initializes the global logger to write log output to a file named "log.txt"
+// in the application's current working directory. If initialization succeeds, it returns nil;
+// otherwise, it returns an error. This function should be called at the very start of the application.
+//
+// The logger is configured to include the date, time, and short file name in each log entry.
 func InitLogger() error {
-	// Use current working directory for log file path
 	cwd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
 	logPath := filepath.Join(cwd, logFileName)
 
-	// Open file in append mode, create if not exists
 	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		return err
 	}
 
-	// Create the logger
 	Log = log.New(file, "APP: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Log.Println("Logger initialized")
 	return nil
