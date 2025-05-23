36aebec977eb7d89f0a30fab0ac53a855e8ca715
mattradial <137439375+mattradial@users.noreply.github.com>
2025-05-23 17:40:44 -0400

    feat: update server to reject non-post requests

--- server/server_test.go
+++ server/server_test.go
@@ -8,10 +8,76 @@ import (
 )
 
 func TestFooHandler(t *testing.T) {
+	var jobHandlerUrl = "http://localhost:8080"
+
+	// Should return method not allowed for GET, PATCH, PUT, DELETE
+	// GET
+	resp, err := http.Get(jobHandlerUrl)
+	if err != nil {
+		t.Fatalf("Request failed: %v", err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf(
+			"Expected status %d for GET, got %d",
+			http.StatusMethodNotAllowed,
+			resp.StatusCode,
+		)
+	}
+
+	// PATCH
+	req, err := http.NewRequest(http.MethodPatch, jobHandlerUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to create Patch request %v", err)
+	}
+	client := &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("Patch request failed %v", err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf(
+			"Expected status %d for PATCH, got %d",
+			http.StatusMethodNotAllowed,
+			resp.StatusCode,
+		)
+	}
+
+	// PUT
+	req, err = http.NewRequest(http.MethodPut, jobHandlerUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to create Put request %v", err)
+	}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("Put request failed %v", err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf(
+			"Expected status %d for PUT, got %d",
+			http.StatusMethodNotAllowed,
+			resp.StatusCode,
+		)
+	}
+
+	// DELETE
+	req, err = http.NewRequest(http.MethodDelete, jobHandlerUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to create Delete request %v", err)
+	}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("Delete request failed %v", err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf(
+			"Expected status %d for DELETE, got %d",
+			http.StatusMethodNotAllowed,
+			resp.StatusCode,
+		)
+	}
 
 	// Why when we declare our json body outside the loop do we only get 1 post
 	// response body
-
 	for i := range 10 {
 		m := Message{i, "Alice", "Hello", 1294706395881547000}
 		b, err := json.Marshal(m)
@@ -21,7 +87,7 @@ func TestFooHandler(t *testing.T) {
 
 		buf := bytes.NewBuffer(b)
 
-		resp, err := http.Post("http://localhost:8080/foo", "application/json", buf)
+		resp, err := http.Post(jobHandlerUrl, "application/json", buf)
 		if err != nil {
 			t.Fatalf("Request failed: %v", err)
 		}
